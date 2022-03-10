692de80f6eb0b93cd27dd3e77d8a2432691f7d6f
solomonade1 <solomonadegboyega1@gmail.com>
2022-03-10 14:20:27 -0800

    Task 6 t0 101 is added

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/** 
+ * main- print the various types of data
+ * return- 0 if exited properly, non zero otherwise
+ */
+int main(void)
+	{
+		int intType;
+		float floatType;
+		double doubleType;
+		char charType;
+
+		printf("Size of int: %zu bytes\n", sizeof(intType));
+		printf("Size of float: %zu bytes\n", sizeof(floatType));
+		printf("Size of double: %zu bytes\n, sizeof(doubleType));
+		printf("Size of char: %zu bytes\n, sizeof(charType));
+
+
+		return(0);
+	}
