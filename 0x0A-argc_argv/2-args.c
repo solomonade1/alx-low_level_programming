ded43c2e53c1ec15dd708f7e1666f3b4879fdc9e
solomonade1 <solomonadegboyega1@gmail.com>
2022-04-01 20:46:06 -0700

    Task 1 and 2 re added

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,10 +9,15 @@
  * Return: 0(Success)
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
+
 {
 	int i;
-	for(i = 0; i < argc; i++)
+
+	for (i = 0; i < argc; i++)
+
+	{
 		printf("%s\n", argv[i]);
-	return 0;
+	}
+	return (0);
 }
